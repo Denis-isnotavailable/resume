a46cf71d50c793bfb56c2944a39f54b76eba9dce
Denis-isnotavailable <92397311+Denis-isnotavailable@users.noreply.github.com>
2025-08-10 21:20:30 +0300

    change styles in project

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -31,35 +31,35 @@ export default async function ProjectPage({ params }: PageProps) {
                 {name}
             </Heading>
             
-            <p className="mb-8 text-lg text-yellow-400 font-bold">
+            <p className="mb-8 text-lg text-yellow-100 font-bold">
                 {allTechnologies}
             </p>
 
-            <div className="mb-10 text-lg font-bold border-b border-b-slate-100 md:flex md:justify-between">
-                <p>{date}</p>
+            <div className="mb-10 text-lg font-bold border-b border-b-slate-100 md:flex md:justify-between">                
                 <Link
                     href={project_live_link}
                     aria-label={name || ""}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="break-words"
+                    className="break-words text-yellow-400 font-bold"
                 >
                     {project_live_link}
                 </Link>
+                <p>{date}</p>
             </div>
 
             <p className="mb-4 text-lg">
                 {role}
             </p>
 
-            {frontRepo && <Link href={frontRepo} className="block mb-4 text-lg text-yellow-100">
+            {frontRepo && <Link href={frontRepo} className="block mb-4 text-lg">
                 <span className="text-lg font-bold text-slate-100">
                     {'Project repository front: '}
                 </span>
                 {frontRepo}
             </Link>}
 
-            {backRepo && <Link href={backRepo} className="block mb-4 text-lg text-yellow-100">
+            {backRepo && <Link href={backRepo} className="block mb-4 text-lg">
                 <span className="text-lg font-bold text-slate-100">
                     {'Project repository back: '}
                 </span>
